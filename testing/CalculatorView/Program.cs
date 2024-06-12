8f6e3c4534639aacc00094967aa80640d9c5a92e
DannyKentala <handres41@outlook.com>
2024-06-12 17:41:55 -0500

    Remove testing

--- testing/CalculatorView/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Calculator;
-
-Console.WriteLine("Hello, World!");
-
-MyCalculator calculator = new();
-int result = calculator.Multiplication(200, 3);
-Console.WriteLine(result);  
